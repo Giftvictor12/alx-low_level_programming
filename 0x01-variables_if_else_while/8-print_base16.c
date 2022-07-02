7d7c6f558711c2e6465273c951f4902e10b66714
Giftvictor12 <giftvictor1212@gmail.com>
2022-07-01 18:41:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,15 +3,17 @@
 #include <stdio.h>
 
 /**
-* main - prints all the numbers of base 16 in lowercase, followed by a new line
+*main - prints all the numbers of base 16 in lowercase, followed by a new line
 *
-* Return: 0
+*Return: Always 0
 **/
 int main(void)
 {
 /**
-* hexadecimal* 0 1 2 3 4 5 6 7 8 9 a c d e f
+* hexadecimal
+* 0 1 2 3 4 5 6 7 8 9 a c d e f
 */
+char ch;
 int n;
 
 for (n = 48; n <= 57; n++)
