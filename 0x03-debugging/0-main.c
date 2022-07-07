e90561cba50b5e1ca17228917aab2620b9539232
Giftvictor12 <giftvictor1212@gmail.com>
2022-07-07 01:25:32 -0700

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,18 +4,13 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-int n;
-if (n > 0)
-printf("%d is positive\n", n);
-if (n == 0)
-printf("%d is zero\n", n);
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
+int i;
+
+i = 0;
+positive_or_negative(i);
+
 return (0);
 }
 
