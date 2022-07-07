60bf91accbf9650255a64354d284d2f80b51e392
Giftvictor12 <giftvictor1212@gmail.com>
2022-07-07 00:47:04 -0700

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,9 +7,18 @@
 
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
+int n;
 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+if (n == 0)
+printf("%d is zero\n", n);
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
 return (0);
 }
+
