181372f696e36bbe7360cc504c12c62355a06035
Giftvictor12 <giftvictor1212@gmail.com>
2022-07-08 02:42:59 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
- * @a: a - variable
- * @b: b - variable
+ * mul - multiplying 2 numbers
+ * @a: integer param1
+ * @b: integer param2
  * Return: success and integer
  */
 int mul(int a, int b)
