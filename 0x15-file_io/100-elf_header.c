cf199f615f6c52c93910bdeb4c8db7c279683585
Giftvictor12 <giftvictor1212@gmail.com>
2022-08-22 18:21:32 -0700

    advanced task done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,71 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <elf.h>
 
-void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
-* main - copies the content of one file to another
-* @argc: argument count
-* @argv: arguments passed
+* get_type - determine file type
+* @ehdr: pointer to struct of ELF header features
+*/
+void get_type(Elf64_Ehdr *ehdr)
+{
+printf("%-35s", "Type:");
+switch (ehdr->e_type)
+{
+case ET_NONE:
+printf("NONE (Unknown type)\n");
+break;
+case ET_REL:
+printf("REL (Relocatable file)\n");
+break;
+case ET_EXEC:
+printf("EXEC (Executable file)\n");
+break;
+case ET_DYN:
+printf("DYN (Shared object file)\n");
+break;
+case ET_CORE:
+printf("CORE (Core file)\n");
+break;
+}
+}
+/**
+* main - entry point for program to get header of ELF file
+* @argc: arg of count
+* @argv: arg of array
 *
-* Return: 1 on success, exit otherwise
+* Return: 1 on success, error code on failure
 */
 int main(int argc, char *argv[])
 {
-int src, dest, n_read = 1024, wrote, close_src, close_dest;
-unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-char buffer[1024];
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
-exit(97);
-}
-src = open(argv[1], O_RDONLY);
-check_IO_stat(src, -1, argv[1], 'O');
-dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-check_IO_stat(dest, -1, argv[2], 'W');
-while (n_read == 1024)
-{
-n_read = read(src, buffer, sizeof(buffer));
-if (n_read == -1)
-check_IO_stat(-1, -1, argv[1], 'O');
-wrote = write(dest, buffer, n_read);
-if (wrote == -1)
-check_IO_stat(-1, -1, argv[2], 'W');
-}
-close_src = close(src);
-check_IO_stat(close_src, src, NULL, 'C');
-close_dest = close(dest);
-check_IO_stat(close_dest, dest, NULL, 'C');
-return (0);
-}
-/**
-* check_IO_stat - checks if a file can be opened or closed
-* @stat: file descriptor of the file to be opened
-* @filename: name of the file
-* @mode: closing or opening
-* @fd: file descriptor
-*
-* Return: void
-*/
-void check_IO_stat(int stat, int fd, char *filename, char mode)
-{
-if (mode == 'C' && stat == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-else if (mode == 'O' && stat == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-exit(98);
-}
-else if (mode == 'W' && stat == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
-}
+printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+return (1);
 }
